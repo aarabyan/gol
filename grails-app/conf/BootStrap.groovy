6f6fffebebbb369225a6064f08977d2d2f83dd33
Aram Arabyan <aarabyan@webbfontaine.am>
2015-05-15 16:24:40 +0400

    create sample of Board in BootStarp

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import com.aram.gol.Board
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        new Board(rows: 20, columns: 20, boardData: [-123, 2, 96, 48, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                                     0, 0, 0, 0, 0, 0, 0, 0]).save flush: true
+
     }
     def destroy = {
     }
